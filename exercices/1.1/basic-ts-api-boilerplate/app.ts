53237877dceacddf5d0abf93eea0fc39d6a52029
jalal-bensalah-vinci <Jalalben0753@gmail.com>
2025-09-17 12:28:13 +0200

    creating 1.1 and cloning boilerplate

---
+++ exercices/1.1/basic-ts-api-boilerplate/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import usersRouter from "./routes/users";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+
+export default app;
---
+++ exercices/1.1/basic-ts-api-boilerplate/types.ts
@@ -0,0 +1,7 @@
+interface Film {
+  id: number;
+  director: string;
+  duration: string;
+}
+
+export type {Film};
