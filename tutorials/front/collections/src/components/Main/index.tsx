5f42dcdf3f0b0be52cf86c55face0f01cb222b45
jalal-bensalah-vinci <Jalalben0753@gmail.com>
2025-10-13 11:55:43 +0200

    tuto: collections

---
+++ tutorials/front/collections/src/components/Main/index.tsx
@@ -0,0 +1,26 @@
+import "./main.css";
+import sound from "../../assets/sounds/Infecticide-11-Pizza-Spinoza.mp3";
+import PizzaMenu from "./PizzaMenu";
+
+
+const Main = () => {
+  return (
+    <main>
+      <p>My HomePage</p>
+      <p>
+        Because we love JS, you can also click on the header to stop / start the
+        music ; )
+      </p>
+      <audio id="audioPlayer" controls autoPlay>
+        <source src={sound} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <table className="pizza-menu">
+        <PizzaMenu />
+      </table>
+    </main>
+  );
+};
+
+
+export default Main;
